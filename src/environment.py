3df82bc5e0b01e74d6a9655264296de416261ae0
William Rågstad <william.ragstad@gmail.com>
2022-04-08 17:20:47 +0200

    Add type hints

--- src/environment.py
+++ src/environment.py
@@ -1,21 +1,24 @@
 
+from .atoms import Atom
+
+
 class Environment():
     """
     The Environment class is used to store the values of
     variables in a scope during the execution of a program.
     """
-    def __init__(self, name, parent):
+    def __init__(self, name: str, parent):
         """
         Initialize an environment with a name and a parent environment.
         """
         self.name = name
-        self.parent = parent
-        self.values = {}
+        self.parent: Environment = parent
+        self.values: dict[str, Atom] = {}
 
-    def set(self, name, value):
+    def set(self, name: str, value: Atom) -> None:
         self.values[name] = value
     
-    def get(self, name):
+    def get(self, name: str) -> Atom | None:
         if name in self.values:
             return self.values[name]
         elif self.parent:
