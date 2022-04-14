6f09313b64f3af1064951ae9f3bb92e81f2da2b6
William Rågstad <william.ragstad@gmail.com>
2022-04-14 16:43:46 +0200

    Update arithmetic.m

--- examples/arithmetic.m
+++ examples/arithmetic.m
@@ -1,11 +1,13 @@
+print("Value", "\t", "Is correct")
 result = 1 + 2 * 3
-print(result, result == 7)
+print(result, "\t", result == 7)
 result = 3 * 2 + 1
-print(result, result == 7)
+print(result, "\t", result == 7)
 result = 7 - 5 + 2
-print(result, result == 4)
+print(result, "\t", result == 4)
 result = (10 + 5) * 3 - 10 / 5 - 5 + 4
-print(result, result == 42)
+print(result, "\t", result == 42)
 result = 5 * 10 + 10 ^ 2 / 20
-print(result, result == 55)
-result
+print(result, "\t", result == 55)
+result = true && false || true && (true && false || false)
+print(result, "\t", result == false)
