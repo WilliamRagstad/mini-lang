4788e6936fbdf73593c53bd09621ccd0b42e99b9
William Rågstad <william.ragstad@gmail.com>
2022-04-14 17:40:27 +0200

    Add test case

--- examples/arithmetic.m
+++ examples/arithmetic.m
@@ -9,5 +9,7 @@ result = (10 + 5) * 3 - 10 / 5 - 5 + 4
 print(result, "\t", result == 42)
 result = 5 * 10 + 10 ^ 2 / 20
 print(result, "\t", result == 55)
+result = -1 - -(2 - -3)
+print(result, "\t", result == 4)
 result = true && false || true && (true && false || false)
 print(result, "\t", result == false)
