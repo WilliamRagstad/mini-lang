fe4ab45fc0698a8cf8bfd5fc26e07505b49446e2
William Rågstad <william.ragstad@gmail.com>
2022-04-14 11:46:57 +0200

    Update arithmetic.m

--- examples/arithmetic.m
+++ examples/arithmetic.m
@@ -2,6 +2,10 @@ result = 1 + 2 * 3
 print(result, result == 7)
 result = 3 * 2 + 1
 print(result, result == 7)
+result = 7 - 5 + 2
+print(result, result == 4)
 result = (10 + 5) * 3 - 10 / 5 - 5 + 4
 print(result, result == 42)
+result = 5 * 10 + 10 ^ 2 / 20
+print(result, result == 55)
 result
