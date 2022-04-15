770cb435c53502f5add04880ef75965e31ec3ee1
William Rågstad <william.ragstad@gmail.com>
2022-04-15 17:17:50 +0200

    Update tuples.m

--- examples/tuples.m
+++ examples/tuples.m
@@ -17,3 +17,8 @@ print(c + " + (5, 2) =", c + (5, 2))
 e = ("test", true)
 f = (1, '!')
 print(e + " + " + f + " =", e + f)
+
+print("first index in e:", e[0])
+print("second index in e:", e[1])
+print("first index in f:", f[0])
+print("second index in f:", f[1])
