2ee8514980e8fc7f70e5328db341425f41cfb09b
William Rågstad <william.ragstad@gmail.com>
2022-04-18 18:41:04 +0200

    Update indexing.m

--- examples/indexing.m
+++ examples/indexing.m
@@ -4,3 +4,9 @@ print(a['b']['c'] + 2)
 
 b = [1, 2, 3]
 print(b[1] * b[1] * b[1] * (b[1] + b[2]) + b[2] - b[0])
+
+ns = #{func: (x) => x * 2 + 5}
+print(ns.func(5))
+
+l = [1, 2, 3][2]
+print(l, l == 3)
