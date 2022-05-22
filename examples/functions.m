d60de64b745c7f8fcc8f714d4b3274a41f01e1a8
William Rågstad <william.ragstad@gmail.com>
2022-05-22 13:55:05 +0200

    Add more examples for lambdas

--- examples/functions.m
+++ examples/functions.m
@@ -13,3 +13,14 @@ f(x) = 2*x + 1
 print(f(2))
 
 print( (() => "IIFE")() + " works!" )
+
+// Lambdas
+a = () => 1
+b = x => 5
+c = (y) => 6
+d = (x, y) => x + y
+e = (x, y) => {
+	z = x + y
+	z * z
+}
+print(a(), b(1), c(2), d(3, 4), d(5, 6))
