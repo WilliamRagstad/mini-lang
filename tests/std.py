93e7630e9cfdc9e71a31d3c4001f65c50717e08b
William Rågstad <william.ragstad@gmail.com>
2022-05-22 21:24:51 +0200

    Expand standard library

--- tests/std.py
+++ tests/std.py
@@ -1,17 +1,22 @@
 from .util import assert_eval, get_all_asserts_passed, new_test_suite, ValueAtom
 
-def test_print():
-    print("- Testing print function")
-    assert_eval("print(\"Hello World\")", ValueAtom("unit", None))
-
-def test_input():
-    print("- Testing input function")
-    assert_eval("input(\"What is your name?\")", ValueAtom("string", "John"))
 
 def run_all() -> bool:
     new_test_suite("standard library")
-    test_print()
-    test_input()
+    print("- Testing print function")
+    assert_eval("print(\"Hello World\")", ValueAtom("unit", None))
+    print("- Testing input function")
+    # assert_eval("input(\"What is your name?\")", ValueAtom("string", "John"))
+    print("(skipped)")
+    print("- Testing exit function")
+    # assert_eval("exit(11)", ValueAtom("unit", None))
+    print("(skipped)")
+    print("- Testing system run function")
+    assert_eval("system_run(\"echo Hello World\", true)", ValueAtom("unit", None))
+    print("- Testing system output function")
+    assert_eval("system_output(\"echo Hello World\", true)", ValueAtom("string", "Hello World\n"))
+    print("- Testing file read function")
+
     return get_all_asserts_passed()
 
 if __name__ == "__main__":
