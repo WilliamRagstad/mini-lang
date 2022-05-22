ea1eef03d90ab87ba31e7185b6bb3fadbc475dab
William Rågstad <william.ragstad@gmail.com>
2022-05-22 16:51:06 +0200

    Improved tests

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 from tests.objects import run_all as run_all_object_tests
+from tests.lists import run_all as run_all_list_tests
 
 def main():
     # run all tests
+    passed = True
     print("\nRunning all tests:")
-    print("==================\n")
-    run_all_object_tests()
+    print("==================")
+    passed &= run_all_object_tests()
+    passed &= run_all_list_tests()
     print("\n==================")
-    print("\nOK! All tests passed.")
+    if passed:
+        print("\nOK! All tests passed.")
+    else:
+        print("\nERROR! Some tests failed.")
 
 if __name__ == "__main__":
     main()
--- tests/util.py
+++ tests/util.py
@@ -2,7 +2,29 @@ from io import StringIO
 from src.atoms import Atom, ValueAtom
 from src.interpreter import execute, globalEnvironment
 
+all_asserts_passed = True
+
 def eval(input: str) -> Atom:
     res, _ = execute(StringIO(input), globalEnvironment())
     return res
 
+def assert_eval(input: str, expected: Atom) -> None:
+    global all_asserts_passed
+    try:
+        actual = eval(input)
+        if actual != expected:
+            all_asserts_passed = False
+            print(f"  - FAILED AT: {input}")
+            print(f"    Expected: {expected}, got: {actual}")
+    except Exception as e:
+        all_asserts_passed = False
+        print(f"  - FAILED AT: {input}")
+        print(f"    Exception: {e}")
+
+def get_all_asserts_passed() -> bool:
+    return all_asserts_passed
+
+def new_test_suite(forTests: str) -> None:
+    global all_asserts_passed
+    all_asserts_passed = True
+    print(f"\nRunning {forTests} tests:")
