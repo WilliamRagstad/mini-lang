fc03f36b004acd611411c5d118d2bfa409ad1769
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-05-31 21:46:44 +0200

    Add compile option

--- mini.py
+++ mini.py
@@ -9,11 +9,14 @@ Usage: mini (options) [file.mini]
 Options:
     -h, --help      Print this help message and exit
     -r, --repl      Start the REPL
-    --debug     Enable debug mode
+    -c, --compile   Compile to a standalone executable
+    --debug         Enable debug mode
 
 Examples:
-    mini -r         Enter the REPL
-    mini file.mini  Evaluate the input file\n"""
+    mini -r             Enter the REPL
+    mini file.mini      Interpret/Evaluate the input file
+    mini -c file.mini   Compile the input file
+\n"""
 
 # === Helper functions ===
 def print_error(msg: str):
@@ -22,24 +25,33 @@ def print_error(msg: str):
 
 # === Main ===
 def main(args: list):
-    debug = False
+    options = {
+        "debug": False,
+        "compile": False,
+    }
     if '--debug' in args:
-        debug = True
+        options['debug'] = True
         args.remove('--debug')
     if len(args) == 0 or '-h' in args or '--help' in args:
         print(USAGE)
         sys.exit(0)
     elif '-r' in args or '--repl' in args:
-        repl(debug)
+        repl(options)
+    elif '-c' in args or '--compile' in args:
+        if len(args) != 2:
+            print_error("-c or --compile requires exactly one argument.")
+        options['compile'] = True
+        interpret(args[1], options)
+
 
     # Evaluate
     elif len(args) > 1:
         print_error("Too many arguments")
     elif len(args) == 1:
-        interpret(args[-1], debug)
+        interpret(args[-1], options)
     else:
         print_error("Unknown option")
 
 if __name__ == '__main__':
-    # Call main with args
-    main(sys.argv[1:])
+  # Call main with args
+  main(sys.argv[1:])
