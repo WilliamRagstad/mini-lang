944ecd2d86f676d43510c04db2c4acdecf16f8eb
William Rågstad <william.ragstad@gmail.com>
2023-12-05 18:23:45 +0100

    Add example files to test suite

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from tests.util import done, set_crash_on_error
 from tests.map import run_all as run_all_map_tests
 from tests.lists import run_all as run_all_list_tests
 from tests.std import run_all as run_all_std_tests
+from tests.examples import run_all as run_all_examples
 
 def main():
     # run all tests
@@ -12,7 +13,7 @@ def main():
     passed &= run_all_map_tests()
     passed &= run_all_list_tests()
     passed &= run_all_std_tests()
-    print("\n==================")
+    passed &= run_all_examples()
     done(passed)
 
 if __name__ == "__main__":
---
+++ tests/examples.py
@@ -0,0 +1,39 @@
+from os import listdir
+from os.path import isfile, join
+import subprocess
+import sys
+
+from tests.util import done
+
+def run_file(file: str) -> bool:
+    """
+    run a file in the /examples directory
+    using the command line interface:
+    `python3 mini.py examples/<file>`
+    """
+    python = sys.orig_argv[0]
+    try:
+        subprocess.check_output([python, "mini.py", f"examples/{file}"])
+        return True
+    except subprocess.CalledProcessError as e:
+        print(e.output.decode("utf-8"))
+        return False
+
+def run_all() -> bool:
+    print("\nRunning examples tests:")
+    # run all files in the /examples directory
+    passed = True
+    files = [f for f in listdir("examples") if isfile(join("examples", f))]
+    for file in files:
+        if file.endswith(".m"):
+            print(f"- examples/{file}")
+            match file:
+                case "input.m":
+                    print("(skipped)")
+                    continue
+                case _:
+                    passed &= run_file(file)
+    return passed
+
+if __name__ == "__main__":
+    done(run_all())
